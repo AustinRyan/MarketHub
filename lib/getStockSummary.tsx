36a468b738b53b5bc9542439c1c5eaaf74f360f1
Austin Ryan <austin.ryan@nasa.gov>
2023-09-21 14:29:54 -0400

    added try catch and a manual cache solution for getStockSummary API fetching

--- lib/getStockSummary.tsx
+++ lib/getStockSummary.tsx
@@ -1,37 +1,57 @@
 import Bottleneck from "bottleneck";
 
 const limiter = new Bottleneck({
-	minTime: 1000,
+	minTime: 1000, // Adjust this value according to the API's rate limit
 });
 
+const cache: any = {};
+
 export default async function getStockSummary(ticker: string) {
-	return limiter.schedule(() => {
-		let rapidApiKey = process.env.NEXT_PUBLIC_RAPID_API_KEY;
-		if (!rapidApiKey) {
-			throw new Error("Missing RAPIDAPI_KEY environment variable");
-		}
+	const cachedData = cache[ticker];
+	const currentTime = Date.now();
 
-		const options = {
-			method: "GET",
-			params: {
-				symbol: ticker,
-				region: "US",
-			},
-			headers: {
-				"content-type": "application/json",
-				"X-RapidAPI-Key": rapidApiKey,
-				"X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
-			},
-			next: {
-				revalidate: 60,
-			},
-		};
+	// Serve cached data if available and not expired
+	if (cachedData && currentTime - cachedData.timestamp < 60 * 1000) {
+		return cachedData.data;
+	}
 
-		const url = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary?symbol=${ticker}&region=US`;
+	try {
+		return limiter.schedule(() => {
+			let rapidApiKey = process.env.NEXT_PUBLIC_RAPID_API_KEY;
+			if (!rapidApiKey) {
+				throw new Error("Missing RAPIDAPI_KEY environment variable");
+			}
 
-		return fetch(url, options).then((res) => {
-			if (!res.ok) throw new Error(res.statusText);
-			return res.json();
+			const options = {
+				method: "GET",
+				params: {
+					symbol: ticker,
+					region: "US",
+				},
+				headers: {
+					"content-type": "application/json",
+					"X-RapidAPI-Key": rapidApiKey,
+					"X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+				},
+				next: {
+					revalidate: 60,
+				},
+			};
+
+			const url = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary?symbol=${ticker}&region=US`;
+
+			return fetch(url, options).then((res) => {
+				if (!res.ok) throw new Error(res.statusText);
+				return res.json().then((data) => {
+					// Store the fetched data in the cache with a timestamp
+					cache[ticker] = { data, timestamp: currentTime };
+					return data;
+				});
+			});
 		});
-	});
+	} catch (error) {
+		// Handle the error, possibly by logging it and rethrowing it
+		console.error("Error fetching stock summary", error);
+		throw error;
+	}
 }

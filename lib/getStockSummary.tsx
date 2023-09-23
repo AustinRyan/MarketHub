27c69bb02c098dd8be97f0c3503bdee73f72dd70
Austin Ryan <austin.ryan@nasa.gov>
2023-09-23 19:43:47 -0400

    adding exponential backoff for now only

--- lib/getStockSummary.tsx
+++ lib/getStockSummary.tsx
@@ -1,12 +1,13 @@
 import limiter from "./apiLimiter";
 
 const cache: any = {};
+const maxRetries = 5; // Maximum number of retries
+const delayFactor = 2; // Delay factor for exponential backoff
 
 export default async function getStockSummary(ticker: string): Promise<any> {
 	const currentTime = Date.now();
 	const cachedData = cache[ticker];
 
-	// If data is in cache and not expired, return cached data
 	if (cachedData && currentTime - cachedData.timestamp < 60 * 1000) {
 		console.log(
 			`Cache hit for ${ticker} at ${new Date(currentTime).toISOString()}`
@@ -14,51 +15,49 @@ export default async function getStockSummary(ticker: string): Promise<any> {
 		return cachedData.data;
 	}
 
-	try {
-		return await limiter.schedule(async () => {
-			let rapidApiKey = process.env.NEXT_PUBLIC_RAPID_API_KEY;
-			if (!rapidApiKey) {
-				throw new Error("Missing RAPIDAPI_KEY environment variable");
-			}
+	let retries = 0;
+	while (retries < maxRetries) {
+		try {
+			const data = await limiter.schedule(async () => {
+				let rapidApiKey = process.env.NEXT_PUBLIC_RAPID_API_KEY;
+				if (!rapidApiKey)
+					throw new Error("Missing RAPIDAPI_KEY environment variable");
 
-			const options = {
-				method: "GET",
-				headers: {
-					"content-type": "application/json",
-					"X-RapidAPI-Key": rapidApiKey,
-					"X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
-				},
-			};
+				const options = {
+					method: "GET",
+					headers: {
+						"content-type": "application/json",
+						"X-RapidAPI-Key": rapidApiKey,
+						"X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+					},
+				};
 
-			const url = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary?symbol=${ticker}&region=US`;
+				const url = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary?symbol=${ticker}&region=US`;
+				const res = await fetch(url, options);
 
-			const res = await fetch(url, options);
+				if (!res.ok)
+					throw new Error(
+						`Failed to fetch stock summary for ${ticker}. Status: ${res.status}`
+					);
+				return await res.json();
+			});
 
-			console.log(
-				`API call for ${ticker} at ${new Date(currentTime).toISOString()}`
-			);
-
-			if (res.status === 429) {
-				// Implement retry logic with exponential backoff
-				console.warn("429 error, retrying with backoff...");
-				await new Promise((res) => setTimeout(res, 2000)); // Example: wait for 2 seconds before retrying
-				return getStockSummary(ticker); // Retry the same function
-			}
-
-			if (!res.ok) {
-				throw new Error(
-					`Failed to fetch stock summary for ${ticker}. Status: ${res.status}`
-				);
-			}
-
-			const data = await res.json();
-
-			// Store the fetched data in the cache with a timestamp
 			cache[ticker] = { data, timestamp: currentTime };
 			return data;
-		});
-	} catch (error) {
-		console.error(`Error fetching stock summary for ${ticker}:`, error);
-		throw error;
+		} catch (error: any) {
+			if (error.status === 429 && retries < maxRetries) {
+				retries++;
+				const delay = Math.pow(delayFactor, retries) * 1000; // Exponential backoff
+				console.error(`429 error, retrying with backoff... Delay: ${delay} ms`);
+				await new Promise((res) => setTimeout(res, delay)); // Wait before retrying
+			} else {
+				console.error(`Error fetching stock summary for ${ticker}:`, error);
+				throw error;
+			}
+		}
 	}
+
+	throw new Error(
+		`Failed to fetch stock summary for ${ticker} after ${maxRetries} retries.`
+	);
 }
